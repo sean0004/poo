6568724271af1be237421ec5946649ef3fa837d9
sean0004 <seanvalencia64@gmail.com>
2025-10-15 21:56:57 +0000

    momento2

---
+++ clase7/persona.java
@@ -0,0 +1,44 @@
+public class persona {
+
+ //Atributos
+ private int identificacion;
+ private string nombre;
+ private string apellidos;
+ private int edad;
+ private double estructura;
+ private double peso;
+
+ //metodo constructor de la clase se econoce porque tiene el mismo nombre de la clase
+ //El constructor de la clase me permite inicializar la clase
+ //La palabra reservada "this" se utiliza para autoreferenciar el atributo de la clase
+ public persona(int identificacion, string nombres, stirng apellidos, int edad, double estructura, double peso){
+    this.identificacion= identificacion
+    this.nombres = nombres
+    this.apellidos= apellidos
+    this.edad=edad
+    this.estructura=estructura
+    this.peso=peso
+ }
+
+ //metodos
+ public void caminar(){
+    system.out.println(x:"caminar");
+ }
+     
+ public boolean jugar(){
+    return true;
+ }
+
+ public void comer(){
+    System.err.println(x:comiendo);
+ }
+ public string estudiar(){
+    System.out.println(x:estudiando);
+ }
+
+ public boolean jugar(){}
+ public void comer(){}
+ public string estudiar(){}
+
+ }
+
